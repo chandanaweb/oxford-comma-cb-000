7113a1c342c6b2348c64703d40993dc8ba6f26c9
chandana M <chandu11288@gmail.com>
2018-02-09 14:25:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   if(array.length ==1)
     array.join
   elsif(array.length == 2)
-  array.join(" and ")
-else
-  array[array.length-1] = "and " << array[array.length-1]
-  array.join(", ")
-end
+    array.join(" and ")
+  else
+    array[array.length-1] = "and " << array[array.length-1]
+    array.join(", ")
+  end
 end
